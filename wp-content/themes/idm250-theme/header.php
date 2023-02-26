e4fb23ed43e69b6a5a070da0eb52b4b5e71e8e1a
Dane LeGassick <danelegassick@gmail.com>
2023-02-26 17:43:30 -0500

    Double Test

--- wp-content/themes/idm250-theme/header.php
+++ wp-content/themes/idm250-theme/header.php
@@ -13,13 +13,14 @@
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 <div class="nav">
-<a href="<?php echo get_template_directory_uri(); ?>/">
-<img src="<?php echo get_template_directory_uri(); ?>/dist/images/logo.png" class="logo"alt="Logo">
-</a>
-<?php wp_nav_menu(['theme_location' => 'primary-menu']); ?>
-<div class="hamburger">
-            <div class="line line1"></div>
-            <div class="line line2"></div>
-            <div class="line line3"></div>
-          </div>
+    <a href="<?php echo get_template_directory_uri(); ?>/front-page.php">
+    <img src="<?php echo get_template_directory_uri(); ?>/dist/images/logo.png" class="logo"alt="Logo">
+    </a>
+    <?php wp_nav_menu(['theme_location' => 'primary-menu']); ?>
+    <div class="hamburger">
+                <div class="line line1"></div>
+                <div class="line line2"></div>
+                <div class="line line3"></div>
+    </div>
 </div>
+
