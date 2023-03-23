4a7c9f157a1fc6b6ee9ea1c1cac81fe7396e7d45
Dane LeGassick <danelegassick@gmail.com>
2023-03-23 12:31:30 -0700

    Add Favicon

--- wp-content/themes/idm250-theme/header.php
+++ wp-content/themes/idm250-theme/header.php
@@ -5,6 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php bloginfo('name'); ?><?php wp_title(); ?></title>
+    <link rel="icon"
+    href="<?php echo get_template_directory_uri(); ?>/dist/images/logo.png"
+    type="image/x-icon" />
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
